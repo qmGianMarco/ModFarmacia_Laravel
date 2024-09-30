cb5af051bc999307c8290a26d7fa1976402df94b
qmGianMarco <gian30marco12@gmail.com>
2024-09-30 16:03:50 -0500

    falto 1 archivo otra vez :'v

--- app/Models/Medicamento.php
+++ app/Models/Medicamento.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Medicamento extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'nombre',
+        'principio_activo',
+        'forma_farmaceutica',
+        'concentracion',
+        'precio_unitario',
+        'stock_min',
+        'canidad'
+
+    ];
 }
